e1a9f7ac3ead749c86e02f423d3c9d952926b3d1
RubenCardozo <cardozoruben@gmail.com>
2017-12-06 16:02:57 +0100

    WebApi

---
+++ MvcDAO/MvcWebRest/Controllers/ProductController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace MvcWebRest.Controllers
+{
+    public class ProductController : ApiController
+    {
+
+        NorthwindEntities entities = new NorthwindEntities(true);
+
+        // GET api/values
+        public IEnumerable<Product> Get()
+        {
+            return entities.Products.ToList();
+        }
+
+        // GET api/Product/5
+        public Product Get(int id)
+        {
+            return entities.Products.FirstOrDefault(p => p.ProductID == id);
+        }
+
+        // GET api/Product/?name=Chai
+        public Product Get(string name)
+        {
+            return entities.Products.FirstOrDefault(p =>String.Compare( p.ProductName, name,true)==0);
+        }
+
+        // POST(create) api/values
+        public void Post([FromBody]Product value)
+        {
+            entities.Products.Attach(value);
+            entities.Entry(value).State = System.Data.EntityState.Added;
+            entities.SaveChanges();
+        }
+
+        // PUT(edit) api/values/5
+        public void Put(int id, [FromBody]Product value)
+        {
+            entities.Products.Attach(value);
+            entities.Entry(value).State = System.Data.EntityState.Modified;
+            entities.SaveChanges();
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+            Product prod = entities.Products.FirstOrDefault(p => p.ProductID == id);
+            entities.Products.Remove(prod);
+            entities.SaveChanges();
+        }
+
+    }
+}
---
+++ MvcDAO/MvcWebRest/Models/Northwind.Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MvcWebRest
+{
+    public partial class NorthwindEntities : DbContext
+    {
+        public NorthwindEntities(bool lazy)
+            : base("name=NorthwindEntities")
+        {
+            if (lazy)
+            {
+                Configuration.LazyLoadingEnabled = true;
+                Configuration.ProxyCreationEnabled = false;
+            }
+        }
+    }
+}
