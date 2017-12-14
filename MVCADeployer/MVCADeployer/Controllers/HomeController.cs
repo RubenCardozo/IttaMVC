40e2b5e98a05431a3ee6e475ebc4ce6198c008a5
RubenCardozo <cardozoruben@gmail.com>
2017-12-14 16:40:32 +0100

    Traduction 2

--- MVCADeployer/MVCADeployer/Controllers/HomeController.cs
+++ MVCADeployer/MVCADeployer/Controllers/HomeController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Threading;
+using System.Globalization;
+using System.Web.Routing;
 
 namespace MVCADeployer.Controllers
 {
@@ -10,9 +13,40 @@ namespace MVCADeployer.Controllers
     {
         //
         // GET: /Home/
-
-        public ActionResult Index()
+        protected override IAsyncResult BeginExecuteCore(AsyncCallback callback, object state)
         {
+            if (Request.QueryString["langue"] != null)
+            {
+                HttpCookie cl = new HttpCookie("langue", Request.QueryString["langue"]);
+                cl.Expires = DateTime.Now.AddYears(1);
+                Response.Cookies.Add(cl);
+                Thread.CurrentThread.CurrentUICulture =
+                Thread.CurrentThread.CurrentCulture
+                = new CultureInfo(Request.QueryString["langue"]);
+            }
+            else
+            if (Request.Cookies["langue"] != null)
+            {
+                Thread.CurrentThread.CurrentUICulture =
+                Thread.CurrentThread.CurrentCulture
+                = new CultureInfo(Request.Cookies["langue"].Value);
+            }
+
+            return base.BeginExecuteCore(callback, state);
+        }
+
+        public ActionResult Index(string lang=null)
+        {
+            if (lang != null)
+            {
+                HttpCookie cl = new HttpCookie("langue",lang);
+                cl.Expires = DateTime.Now.AddYears(1);
+                Response.Cookies.Add(cl);
+                Thread.CurrentThread.CurrentUICulture =
+                Thread.CurrentThread.CurrentCulture 
+                = new CultureInfo(lang);
+            }
+          
             return View();
         }
 
